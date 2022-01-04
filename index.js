e9e1eba5afd66583e4f3210793c1758e691eaea0
Renato <renato_181@hotmail.com>
2022-01-04 13:00:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ app.use(cors());
 
 let auth = require("./auth")(app);
 
-mongoose.connect( process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', 
 // { useNewUrlParser: true, 
 //  useUnifiedTopology: true });
